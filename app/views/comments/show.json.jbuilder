ad7eb185394e8027a3e85e5e87e63200a79ed610
Craig <craig.frey@hotmail.com>
2013-08-22 22:17:30 -0500

    adds comment resource

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :text, :user_id, :picture_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130823031528) do
+ActiveRecord::Schema.define(version: 20130823031656) do
+
+  create_table "comments", force: true do |t|
+    t.string   "text"
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "homes", force: true do |t|
     t.string   "name"
